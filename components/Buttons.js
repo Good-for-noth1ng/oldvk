9de122b5acf251dfa8e91538035a365dd963afd6
Good-for-noth1ng <apasnost@list.ru>
2022-08-16 00:04:20 +0200

    webview demp

--- components/Buttons.js
+++ components/Buttons.js
@@ -1,9 +1,25 @@
-import React from 'react'
-import { Text, View, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { Text, View, TouchableOpacity, Dimensions } from 'react-native'
+import { WebView } from 'react-native-webview'
 import { SIZES, COLORS } from '../constants/theme'
-
+import { APP_ID, PERMISION_CODE, REDIRECT_URI } from '@env'
 export const LoginButton = ({buttonText, handlePress}) => {
+  const [openLogin, setOpenLogin] = useState(false)
+  
+  const onNavChange = (navigationState) => {
+    console.log(navigationState.url)
+  }
+  
   return (
+    openLogin ? 
+    <View style={{flex: 1}}>
+      <WebView 
+        source={{uri: `https://oauth.vk.com/authorize?client_id=${APP_ID}&display=mobile&redirect_uri=${REDIRECT_URI}&scope=${PERMISION_CODE}&response_type=token&v=5.131&revoke=1`}}
+        onNavigationStateChange={onNavChange}
+        style={{height: Dimensions.get('window').height, width: Dimensions.get('window').width}}
+      /> 
+    </View> :
+    (
     <TouchableOpacity style={{
       width: 170,
       height: 45,
@@ -11,7 +27,7 @@ export const LoginButton = ({buttonText, handlePress}) => {
       borderRadius: 5,
       padding: 5,
       alignContent: 'center'
-    }} onPress={() => handlePress()}>
+    }} onPress={() => setOpenLogin(true)}>
       <Text style={{
         fontFamily: 'sans-serif',
         fontSize: SIZES.small + 2,
@@ -20,6 +36,6 @@ export const LoginButton = ({buttonText, handlePress}) => {
       }}>
         {buttonText}
       </Text>
-    </TouchableOpacity>
+    </TouchableOpacity>)
   )
 }
--- screens/Login.js
+++ screens/Login.js
@@ -1,21 +1,53 @@
 import React, { useEffect } from 'react'
 import { View, Text, SafeAreaView, StatusBar, StyleSheet } from 'react-native'
-// import { authorize } from 'react-native-app-auth'
+
+import { authorize } from 'react-native-app-auth'
+import * as Linking from 'expo-linking'
 import * as WebBrowser from 'expo-web-browser'
 import { makeRedirectUri, useAuthRequest, ResponseType } from 'expo-auth-session'
 import Header from '../components/Header'
 import { LoginButton } from '../components/Buttons'
 import { SIZES } from '../constants/theme'
-import { APP_ID, PERMISION_CODE } from '@env'
+import { APP_ID, PERMISION_CODE, REDIRECT_URI } from '@env'
 
 WebBrowser.maybeCompleteAuthSession();
 
-const discovery ={
-  authorizationEndpoint: 'https://oauth.vk.com/authorize',
-};
+// const discovery ={
+//   authorizationEndpoint: 'https://oauth.vk.com/authorize',
+// };
 
-const Login = () => {
+const Login = () => { 
+  // const openAuthSession = async () => {
+  //   WebBrowser.openAuthSessionAsync(
+  //     `https://oauth.vk.com/authorize?client_id=${APP_ID}&display=mobile&redirect_uri=${REDIRECT_URI}&scope=${PERMISION_CODE}&response_type=token&v=5.131&revoke=1`
+  //   ).then(result => console.log(result.url))
+  // }
+  // const handleRedirect = (event) => {
+  //   // removeLinkingListener()
+  //   let data = Linking.parse(event.url)
+  //   console.log(data)
+  // }
+  // const addLinkingListener = () => {
+  //   Linking.addEventListener('url', handleRedirect())
+  // }
+  // const removeLinkingListener = () => {
+  //   Linking.removeEventListener('url', handleRedirect())
+  // } 
+
+  // const openBrowserAsync = async () => {
+  //   try {
+  //     addLinkingListener()
+  //     WebBrowser.openBrowserAsync(
+  //       `https://oauth.vk.com/authorize?client_id=${APP_ID}&display=mobile&redirect_uri=https://oauth.vk.com/blank.html&scope=22&response_type=token&v=5.131`
+  //     ).then(result => console.log(result)) 
+  //   } catch (error) {
+  //     console.log(error)   
+  //   }
+  // }
+
   
+
+
   // const config = {
   //   issuer: 'https://oauth.vk.com/authorize',
   //   client_id: APP_ID,
@@ -27,23 +59,23 @@ const Login = () => {
   //   revoke: '1'
   // }
 
-  const [request, response, promptAsync] = useAuthRequest(
-    {
-      client_id: APP_ID,
-      redirect_uri: makeRedirectUri({
-        scheme: 'oldvk'
-      }),
-      display: 'mobile',
-      scope: PERMISION_CODE,
-      state: '12398',
-      response_type: 'token',
-      revoke: '1'
-    }, 
-    discovery
-  );
-  useEffect(() => {
-    console.log(response.params)
-  }, [response])
+  // const [request, response, promptAsync] = useAuthRequest(
+  //   {
+  //     client_id: APP_ID,
+  //     redirect_uri: makeRedirectUri({
+  //       scheme: 'oldvk'
+  //     }),
+  //     display: 'mobile',
+  //     scope: PERMISION_CODE,
+  //     state: '12398',
+  //     response_type: 'token',
+  //     revoke: '1'
+  //   }, 
+  //   discovery
+  // );
+  // useEffect(() => {
+  //   console.log(response.params)
+  // }, [response])
   // const openAuthSession = async () => {
   //   const result = await authorize(config)
   //   console.log(result.accessToken)
@@ -51,13 +83,14 @@ const Login = () => {
 
   // const openAuthSession = async () => {
   //   try {
-  //     const url = `https://oauth.vk.com/authorize?client_id=${APP_ID}&display=mobile&redirect_uri=https://oauth.vk.com/blank.html/callback&scope=${PERMISION_CODE}&response_type=token&v=5.131`
+  //     const url = `https://oauth.vk.com/authorize?client_id=${APP_ID}&display=mobile&redirect_uri=https://oauth.vk.com/blank.html&scope=${PERMISION_CODE}&response_type=token&v=5.131`
   //     let result = await WebBrowser.openAuthSessionAsync(url) 
   //     console.log(result)
   //   } catch (error) {
   //     console.log(error)
   //   }
   // }
+  
   return (
     <View>
       <SafeAreaView>
@@ -70,7 +103,7 @@ const Login = () => {
               oldvk - мобильное приложение для соцсети вконтакте
             </Text>
           </View>
-          <LoginButton buttonText={'Авторизоваться через vk.com'} handlePress={promptAsync} />
+          <LoginButton buttonText={'Авторизоваться через vk.com'}  />
         </View>
       </SafeAreaView>
     </View>

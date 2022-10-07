e0f8035beb01bf54a80466238bd532615c5c8ec2
Good-for-noth1ng <apasnost@list.ru>
2022-10-07 21:15:29 +0200

    post link

--- components/PostLinks.js
+++ components/PostLinks.js
@@ -1,20 +1,36 @@
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
-
+import Feather from 'react-native-vector-icons/Feather'
+import { COLORS } from '../constants/theme'
 const PostLinks = ({postLinks}) => {
-  const postLink = postLinks[0]
-  const url = postLink.url
-  const title = postLink.title
-  const lastIndex = postLink.sizes.length - 1
-  const photoUrl = postLink.sizes[lastIndex].url
-  return (  
-    <TouchableOpacity activeOpacity={1} style={styles.linkContainer}>
-      <Image source={{uri: photoUrl}} style={styles.linkImageBackground}/>
-      <View>
-        <Text>{title}</Text>
-      </View>
-    </TouchableOpacity>
-  )
+  if (postLinks.length > 0) {
+    const postLink = postLinks[0]
+    const url = postLink.url
+    let title = postLink.title
+    title = title.slice(0, 35)
+    if (title !== postLink.title) {
+      title += '...' 
+    }
+    let address = url
+    address = address.slice(0, 35)
+    if (address !== url) {
+      address += '...' 
+    }
+    return (  
+      <TouchableOpacity style={styles.linkContainer} activeOpacity={1}>
+       <View style={styles.linkIconContainer}>
+         <Feather name='arrow-up-right' size={30} color={COLORS.secondary} />
+       </View>
+       <View style={styles.linkInfoContainer}>
+         <Text style={styles.nameStyle}>{title}</Text>
+         <View style={styles.linkAddressContainer}>
+           <Text style={{marginRight: 4}}>{url}</Text>
+         </View>
+       </View>
+     </TouchableOpacity>
+  )} else {
+    return null
+  }
 }
 
 export default PostLinks
@@ -22,11 +38,31 @@ export default PostLinks
 const styles = StyleSheet.create({
   linkContainer: {
     display: 'flex',
-    width: '100%',
-    height: 300
-  },  
-  linkImageBackground: {
-    width: '100%',
-    height: '100%'
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 5,
+    marginBottom: 5
+  },
+  linkIconContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: 45,
+    height: 45,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: COLORS.light_smoke,
+    borderRadius: 40
+  },
+  linkInfoContainer: {
+    display: 'flex',
+    width: '85%',
+    flexDirection: 'column',
+    marginLeft: 10,
+  },
+  nameStyle: {
+    fontSize: 14,
+  },
+  linkAddressContainer: {
+
   }
 })

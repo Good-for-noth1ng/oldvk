017231a603da07eb874a76536733ca97b0c9ca16
Good-for-noth1ng <apasnost@list.ru>
2022-10-13 21:20:38 +0200

    f

--- components/PostPhotos.js
+++ components/PostPhotos.js
@@ -21,7 +21,6 @@ const PostPhotos = ({postPhotos}) => {
       />
       index += 1
       row.push(image)
-      
     }
     let rowContainer = <View style={{width: '100%'}} key={uuid.v4()}>{row}</View>
     grid.push(rowContainer)

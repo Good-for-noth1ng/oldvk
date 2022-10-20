199eb904d7ea69b5329abaf08ca8a003146055d0
Good-for-noth1ng <apasnost@list.ru>
2022-10-20 19:18:33 +0200

    photo grid

--- components/PostPhotos.js
+++ components/PostPhotos.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View, Image } from 'react-native'
 import React, {useState} from 'react'
 import uuid from 'react-native-uuid';
+import { COLORS } from '../constants/theme';
 
 const PostPhotos = ({postPhotos}) => {
   const imgNum = postPhotos.length
@@ -11,34 +12,30 @@ const PostPhotos = ({postPhotos}) => {
   for (let i = 0; i < rowNum; i++) {
     let row = []
     for (let j = 0; j < 3; j++) {
-      let imgPerRow = imgNum - Math.floor(imgNum / 3) * (i + 1)
+      let imgPerRow = 3
+      if (i == rowNum - 1) {
+        imgPerRow = Math.floor(imgNum / 3)
+      }
+      if (rowNum == 1) {
+        imgPerRow = imgNum
+      }
+      // let imgPerRow = imgNum - Math.floor(imgNum / 3) * (i + 1)
       let width = 100 / imgPerRow + '%'
-      let height = 100 / rowNum + '%'
+      // let height = 100 / rowNum + '%'
       let lastIndexUrl = postPhotos[index]?.sizes.length - 1
-      // console.log(width, height)
+      // console.log(width)
       // console.log(postPhotos[index]?.sizes[lastIndexUrl].url)
       let image = <Image 
         source={{uri: postPhotos[index]?.sizes[lastIndexUrl].url}}
         key={uuid.v4()}
-        style={{width: width, height: height}}
+        style={{width: width, height: 200}}
       />
       index += 1
       row.push(image)
     }
-    let rowContainer = <View style={{width: '100%', display: 'flex', flexDirection: 'row'}} key={uuid.v4()}>{row}</View>
+    let rowContainer = <View style={styles.rowContainer} key={uuid.v4()}>{row}</View>
     grid.push(rowContainer)
   }
-
-
-  // const photos = postPhotos
-  // const renderPhotos = photos.map(photo => (
-  //   <Image 
-  //     source={{uri: photo?.sizes[2].url}} 
-  //     key={uuid.v4()} 
-  //     // style={{width: photo?.sizes[2].width, height: photo?.sizes[2].height}}
-  //     style={{width: '100%', height: 200}}
-  //   />
-  // ))
 
   return (
     <View style={styles.photosContainer}>
@@ -46,11 +43,6 @@ const PostPhotos = ({postPhotos}) => {
         grid && grid
       }
     </View>
-    // <View style={styles.photosContainer}>
-    //     {
-    //       renderPhotos && renderPhotos
-    //     }
-    // </View>
   )
 }
 
@@ -60,5 +52,13 @@ const styles = StyleSheet.create({
   photosContainer: {
     marginBottom: 10,
     marginTop: 10,
+    display: 'flex',
+    flexDirection: 'column',
+    // height: 
+  },
+  rowContainer: {
+    width: '100%', 
+    display: 'flex', 
+    flexDirection: 'row',
   }
 })

dc40aa098ee2b337b8c04cc3fd978eccd8445c31
Good-for-noth1ng <apasnost@list.ru>
2023-09-15 23:35:50 +0200

    f

--- components/CountSortComments.js
+++ components/CountSortComments.js
@@ -3,6 +3,7 @@ import React, { useState} from 'react'
 import FontAwesome from 'react-native-vector-icons/FontAwesome5'
 import { COLORS } from '../constants/theme'
 
+
 const CountSortComments = ({comments, isLightTheme}) => {
   const [commentsCount, setCommentsCount] = useState(comments !== undefined ? comments : 0)
   return (
